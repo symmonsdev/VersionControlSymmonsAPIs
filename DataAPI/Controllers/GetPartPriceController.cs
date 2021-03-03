316893ba5833e9bef8538d16626bdcf362ffe9a6
Mike Martino <mmartino@symmons.com>
2021-03-03 14:58:25 -0500

    add Authorize to all new api controllers

--- DataAPI/Controllers/GetPartPriceController.cs
+++ DataAPI/Controllers/GetPartPriceController.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace DataAPI.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class GetPartPriceController : ControllerBase
--- DataAPI/Controllers/GetPriceGroupController.cs
+++ DataAPI/Controllers/GetPriceGroupController.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace DataAPI.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class GetPriceGroupController : ControllerBase
